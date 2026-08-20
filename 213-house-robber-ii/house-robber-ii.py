6d5a9460115ac0a514afc14d2bb04f918b49bc1e
Selvam Dasari Gnanaprakash <dasariselvam321@gmail.com>
2026-08-20 12:30:48 +0200

    Time: 3 ms (7.23%) | Memory: 19.2 MB (68.92%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,20 +1,26 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        if n==1:
+        if n == 1:
             return nums[0]
-        memo = {}
-        def recursive(index, end):
-            if index > end:
-                return 0
-            if (index, end) in memo:
-                return memo[(index, end)]
-            take = nums[index] + recursive(index+2, end)
-            skip = recursive(index+1, end)
-            memo[(index,end)] = max(take, skip)
-            return memo[(index, end)]
+        if n == 2:
+            return max(nums[0], nums[1])
+
+        def helper(arr):
+            m = len(arr)
+            dp = [0] * (m)
+
+            dp[0] = arr[0]
+            dp[1] = max(arr[0], arr[1])
+
+            for i in range(2, m):
+                take = arr[i] + dp[i-2]
+                skip = dp[i-1]
+
+                dp[i] = max(take, skip)
+            return dp[-1]
         
-        case1 = recursive(0, n-2)
-        case2 = recursive(1,n-1)
-        
+        case1 = helper(nums[1:])
+        case2 = helper(nums[:-1])
+
         return max(case1, case2)
