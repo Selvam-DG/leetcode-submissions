87c11ea5a13e50c1f0ca05a3c3d30e8b2dcf80d2
Selvam Dasari Gnanaprakash <dasariselvam321@gmail.com>
2026-09-06 15:01:15 +0200

    Time: 0 ms (100.00%) | Memory: 19.5 MB (6.29%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,13 +1,17 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # skip  idx+1
+        #skip and start from index+1
+        #oresle take and index+2
+        memo = {}
         n = len(nums)
-        dp = [0] * (n+2)
-
-        for i in range(n-1, -1, -1):
-            take = nums[i] + dp[i+2]
-            skip = dp[i+1]
-
-            dp[i] = max(take, skip)
+        def recursive(index):
+            if index >= n:
+                return 0
+            if index in memo:
+                return memo[index]
+            take = nums[index] + recursive(index+2)
+            skip = recursive(index+1)
+            memo[index] = max(take, skip)
+            return memo[index] 
         
-        return dp[0]
+        return recursive(0)
