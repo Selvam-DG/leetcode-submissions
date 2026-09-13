f321c51ae83810ad78b73e9936897fa0c3bd057b
Selvam Dasari Gnanaprakash <dasariselvam321@gmail.com>
2026-09-13 18:20:12 +0200

    Time: 0 ms (100.00%) | Memory: 19.2 MB (93.17%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,30 +1,20 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        
         if n == 1:
             return nums[0]
-        
         if n == 2:
             return max(nums)
-        
-        def helper(start, end):
-            prev2 = 0
-            prev1 = 0
+        def solve(arr): 
+            n = len(arr)
+            dp = [0] * (n+2)
 
-            for i in range(start, end+1):
-                
-                take = nums[i] + prev2
-                skip = prev1
-
-                curr = max(take, skip)
-
-                prev2 = prev1
-                prev1 = curr
+            for i in range(n-1, -1, -1):
+                dp[i] = max(dp[i+1], arr[i]+dp[i+2])
             
-            return prev1
+            return dp[0]
         
-        case1 = helper(0, n-2)
-        case2 = helper(1, n-1)
+        take_first_house = solve(nums[:-1])
+        skip_first_house = solve(nums[1:])
 
-        return max(case1, case2)
+        return max(take_first_house, skip_first_house)
