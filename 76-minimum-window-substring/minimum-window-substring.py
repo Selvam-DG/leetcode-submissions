1212beccccdee150860035dd8d0cfb6a52ffb919
Selvam Dasari Gnanaprakash <dasariselvam321@gmail.com>
2026-09-13 12:28:44 +0200

    Time: 63 ms (72.99%) | Memory: 19.8 MB (11.91%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,33 +1,35 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         freq_t = dict()
-        for char in t:
-            freq_t[char] = 1 + freq_t.get(char,0)
-        n = len(s)
-        curr_freq = dict()
-        best_length = -1
-        best_start = 0
-        l=0
-        formed = 0
-        for r in range(n):
-            char = s[r]
-            curr_freq [char] = 1 + curr_freq.get(char, 0)
 
-            if char in freq_t and freq_t[char] == curr_freq[char]:
-                formed += 1
+        for char in t:
+            freq_t[char] = 1 + freq_t.get(char, 0)
+        
+        needed = len(freq_t)
+        
+        freq_s = dict()
+        actual = 0
+        start_idx = 0
+        best_length = 0
+        l = 0
+
+        for r in range(len(s)):
+            char = s[r]
+            freq_s[char] = 1 + freq_s.get(char, 0)
+            if char in freq_t and freq_s[char] == freq_t[char]:
+                actual += 1
             
-            while formed == len(freq_t):
-                if best_length == -1 or (r-l+1) < best_length:
+            while needed == actual:
+                if best_length == 0 or (r-l+1) < best_length:
+                    start_idx = l
                     best_length = r-l+1
-                    best_start = l
+
+                leaving_char = s[l]
+                freq_s[leaving_char] -= 1
+                if leaving_char in freq_t and freq_s[leaving_char] < freq_t[leaving_char]:
+                    actual -= 1
                 
-                lchar = s[l]
-                curr_freq[lchar] -= 1
-                if lchar in freq_t and curr_freq[lchar] < freq_t[lchar]:
-                    formed -= 1
                 l += 1
         
-        if best_length == -1:
-            return ""
-        return s[best_start: best_start+best_length]
-
+        return s[start_idx: start_idx+best_length]
+                    
