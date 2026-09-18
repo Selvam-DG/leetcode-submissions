992947c90e0453424923148ca2032cb5d29114e1
Selvam Dasari Gnanaprakash <dasariselvam321@gmail.com>
2026-09-18 22:15:46 +0200

    Time: 183 ms (98.64%) | Memory: 19.4 MB (83.90%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,6 +1,6 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp  = [1] + [0] * amount
+        dp = [1] + [0] * amount
 
         for coin in coins:
             for a in range(coin, amount+1):
