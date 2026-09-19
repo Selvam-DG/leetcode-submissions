f0df644b77548cf1ea01928f262f5c451f9cec61
Selvam Dasari Gnanaprakash <dasariselvam321@gmail.com>
2026-09-19 17:12:25 +0200

    Time: 23 ms (98.15%) | Memory: 22 MB (26.31%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,15 +1,16 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        prefix_sum_count = {0:1}
-        count = 0
+        seen = dict()
+        seen[0] = 1
         curr_sum = 0
+        count = 0
 
-        for i in range(n):
-            curr_sum += nums[i]
-            if (curr_sum - k) in prefix_sum_count:
-                count += prefix_sum_count[curr_sum - k]
-            
-            prefix_sum_count[curr_sum] = 1 + prefix_sum_count.get(curr_sum, 0)
+        for r in range(n):
+            curr_sum += nums[r]
+            need = curr_sum - k
+            if need in seen:
+                count += seen[need]
+            seen[curr_sum] = 1 + seen.get(curr_sum, 0)
         
         return count
