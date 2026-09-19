4b6f4a2f90a59cef9031bbec9288b77c29071041
Selvam Dasari Gnanaprakash <dasariselvam321@gmail.com>
2026-09-19 14:04:32 +0200

    Time: 0 ms (100.00%) | Memory: 19.4 MB (11.65%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,17 +1,22 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        
-        n = len(s)-1
+        n = len(s)
         result = []
-        while n >= 0:
-            while  n >= 0 and s[n] == ' ':
-                n -= 1
-            if n < 0:
+        pointer = n-1
+
+        while pointer >= 0:
+
+            while pointer >= 0 and s[pointer] == ' ':
+                pointer -= 1
+            if pointer < 0:
                 break
-            end = n
-            while n >= 0 and s[n] != ' ':
-                n -= 1
-            start = n+1
-            word = s[start:end+1]
-            result.append(word)
+
+            end = pointer
+            while pointer >= 0 and s[pointer] != ' ':
+                pointer -= 1
+
+            start = pointer+1
+        
+            result.append(s[start:end+1])
+        
         return ' '.join(result)
