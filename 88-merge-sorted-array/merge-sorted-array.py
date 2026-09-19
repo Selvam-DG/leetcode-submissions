a586cbe5d225e1e079efe61dec2963d6c4ec41ec
Selvam Dasari Gnanaprakash <dasariselvam321@gmail.com>
2026-09-19 14:30:27 +0200

    Time: 0 ms (100.00%) | Memory: 19.3 MB (77.86%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,15 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        curr_idx = m+n-1
-        idx1 = m-1
-        idx2 = n-1
+        curr_ptr = m+n-1
+        ptr1 = m-1
+        ptr2 = n-1
 
-        while idx2 >= 0:
-            if idx1 >= 0 and nums1[idx1] > nums2[idx2]:
-                nums1[curr_idx] = nums1[idx1]
-                idx1 -= 1
+        while ptr2 >= 0:
+            if ptr1 >= 0 and nums1[ptr1] >= nums2[ptr2]:
+                nums1[curr_ptr] = nums1[ptr1]
+                ptr1 -=1
             else:
-                nums1[curr_idx] = nums2[idx2]
-                idx2 -= 1
-            curr_idx -= 1
+                nums1[curr_ptr] = nums2[ptr2]
+                ptr2 -= 1
+            curr_ptr -= 1
+        
