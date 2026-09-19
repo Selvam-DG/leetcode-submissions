2f3881f09a9e690a0b4628d3bb6497112aa82766
Selvam Dasari Gnanaprakash <dasariselvam321@gmail.com>
2026-09-19 10:57:55 +0200

    Time: 124 ms (30.75%) | Memory: 35.3 MB (25.53%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -3,25 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        #reverse the list
-        # reverse firt k nums
-        # reverse n-k nums
-        k = k % len(nums)
-        self.reverse(nums)
-        nums[:k] = self.reverse(nums[:k])
-        nums[k:] = self.reverse(nums[k:])
-    
-    
-    def reverse(self, arr):
-        n = len(arr)
+        def reverse(left, right):
+            while left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
 
-        left = 0
-        right = n-1
-
-        while left < right:
-            arr[left], arr[right] = arr[right], arr[left]
-            left += 1
-            right -= 1
-        
-        return arr
-        
+        n = len(nums)
+        k = k % n
+        reverse(0, n-1-k)
+        reverse(n-k, n-1)
+        reverse(0, n-1)
